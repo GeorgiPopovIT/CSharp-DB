eda4607f4ad472f126ed124a127851f36d0c02ec
Georgi Popov <gogopopo262003@gmail.com>
2021-03-01 23:12:42 +0200

    Add exercises from Entity Relations

---
+++ EF CORE/Entity Relations/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+
+        }
+        public StudentSystemContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Resource> Resources { get; set; }
+        public virtual DbSet<Homework> HomeworkSubmissions { get; set; }
+        public virtual DbSet<StudentCourse> StudentCourses { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=Student System;Integrated Security=true;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(x => new { x.StudentId, x.CourseId });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ EF CORE/LINQ/Hospital Database/Models/Patient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Hospital_Database.Models
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        [MaxLength(50)]
+        public string LastName { get; set; }
+    }
+}
